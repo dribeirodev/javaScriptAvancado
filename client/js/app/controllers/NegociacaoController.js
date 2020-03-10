05f4bb262575935b13cd11b2f3697cb807496c19
Diego <diego.souza@ciapetro.com.br>
2020-03-10 17:35:56 -0300

    arrow function

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,9 +5,8 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes(function (model) {
-            this._negociacoesView.update(model);
-        });
+        this._listaNegociacoes = new ListaNegociacoes(model =>
+            this._negociacoesView.update(model)     );
 
         this._negociacoesView = new NegociacaoView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
