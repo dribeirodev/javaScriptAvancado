a2e1897faf38676a43002ff3941a3b54ee4ac387
Diego Ribeiro <diegouploads@gmail.com>
2020-03-10 12:40:05 -0300

    function apagar

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,8 +5,10 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
-        
+        this._listaNegociacoes = new ListaNegociacoes(function (model) {
+            this._negociacoesView.update(model);
+        });
+
         this._negociacoesView = new NegociacaoView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
 
@@ -21,11 +23,16 @@ class NegociacaoController {
 
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._mensagem.text = "Negociação adicionada com sucesso.";
-
         this._mensagemView.update(this._mensagem);
-        this._negociacoesView.update(this._listaNegociacoes);
 
         this._clearForm();
+    }
+
+    apaga() {
+        this._listaNegociacoes.esvazia();
+
+        this._mensagem.texto = 'Negociações apagas com sucesso';
+        this._mensagemView.update(this._mensagem)
     }
 
     _criaNegociacao() {
