2c180ea8acff6e08a19f3dc00784d58ebe04e7f2
Diego Souza <diego.souza@ciapetro.com.br>
2020-10-30 17:06:40 -0300

    Finish

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -9,7 +9,9 @@ class NegociacaoController {
       new ListaNegociacoes(),
       new NegociacaoView($("#negociacoesView")),
       "adiciona",
-      "esvazia"
+      "esvazia",
+      "ordena",
+      "inverteOrdem"
     );
 
     this._mensagem = new Bind(
@@ -17,6 +19,7 @@ class NegociacaoController {
       new MensagemView($("#mensagemView")),
       "text"
     );
+    this._ordemAtual = "";
   }
 
   adiciona(e) {
@@ -24,6 +27,110 @@ class NegociacaoController {
     this._listaNegociacoes.adiciona(this._criaNegociacao());
     this._mensagem.text = "Negociação adicionada com sucesso.";
     this._clearForm();
+  }
+
+  importaNegociacoes() {
+    let service = new NegociacaoService();
+
+    service
+      .obterNegociacoes()
+      .then((negociacoes) => {
+        negociacoes.forEach((negociacao) =>
+          this._listaNegociacoes.adiciona(negociacao)
+        );
+        this._mensagem.texto = "Negociações do período importadas com sucesso";
+      })
+      .catch((error) => (this._mensagem.text = error));
+
+    /* 
+    // Promisse all - tranferido para negociacaoService
+
+    Promise.all([
+      service.obterNegociacoesDaSemana(),
+      service.obterNegociacoesDaSemanaAnterior(),
+      service.obterNegociacoesDaSemanaRetrasada(),
+    ])
+      .then((negociacoes) => {
+        console.log(negociacoes);
+        negociacoes
+          .reduce((arrayAchatado, array) => arrayAchatado.concat(array), [])
+          .forEach((negociacao) => this._listaNegociacoes.adiciona(negociacao));
+        this._mensagem.text = "Negociações importadas com sucesso";
+      })
+      .catch((err) => (this._mensagem.text = err)); */
+
+    /* 
+    
+    // Promisse async
+  
+    service
+      .obterNegociacoesDaSemana()
+      .then((negociacoes) => {
+        negociacoes.forEach((negociacao) =>
+          this._listaNegociacoes.adiciona(negociacao)
+        );
+        this._mensagem.text = "Negociações importadas com sucesso";
+      })
+      .catch((err) => (this._mensagem.text = err));
+
+    service
+      .obterNegociacoesDaSemanaAnterior()
+      .then((negociacoes) => {
+        negociacoes.forEach((negociacao) =>
+          this._listaNegociacoes.adiciona(negociacao)
+        );
+        this._mensagem.text = "Negociações importadas com sucesso";
+      })
+      .catch((err) => (this._mensagem.text = err));
+
+    service
+      .obterNegociacoesDaSemanaRetrasada()
+      .then((negociacoes) => {
+        negociacoes.forEach((negociacao) =>
+          this._listaNegociacoes.adiciona(negociacao)
+        );
+        this._mensagem.text = "Negociações importadas com sucesso";
+      })
+      .catch((err) => (this._mensagem.text = err)); */
+
+    /* 
+    // abaixo modelo com callback
+
+    service.obterNegociacoesDaSemana((err, negociacoes) => {
+      if (err) {
+        this._mensagem.text = err;
+        return;
+      }
+
+      negociacoes.forEach((negociacao) =>
+        this._listaNegociacoes.adiciona(negociacao)
+      );
+      this._mensagem.text = "Negociações importadas com sucesso";
+    });
+
+    service.obterNegociacoesDaSemanaAnterior((err, negociacoes) => {
+      if (err) {
+        this._mensagem.text = err;
+        return;
+      }
+
+      negociacoes.forEach((negociacao) =>
+        this._listaNegociacoes.adiciona(negociacao)
+      );
+      this._mensagem.text = "Negociações importadas com sucesso";
+    });
+
+    service.obterNegociacoesDaSemanaRetrasada((err, negociacoes) => {
+      if (err) {
+        this._mensagem.text = err;
+        return;
+      }
+
+      negociacoes.forEach((negociacao) =>
+        this._listaNegociacoes.adiciona(negociacao)
+      );
+      this._mensagem.text = "Negociações importadas com sucesso";
+    }); */
   }
 
   apaga() {
@@ -45,5 +152,14 @@ class NegociacaoController {
     this._inputValor.value = 0.0;
 
     this._inputData.focus();
+  }
+
+  ordena(coluna) {
+    if (this._ordemAtual == coluna) {
+      this._listaNegociacoes.inverteOrdem();
+    } else {
+      this._listaNegociacoes.ordena((a, b) => a[coluna] - b[coluna]);
+    }
+    this._ordemAtual = coluna;
   }
 }
--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -1,40 +1,52 @@
 class NegociacaoView extends View {
-    template(model) {
-        return `
+  constructor(elemento) {
+    super(elemento);
+  }
+
+  template(model) {
+    return `
         <table class="table table-hover table-bordered">
             <thead>
                 <tr>
-                    <th>DATA</th>
-                    <th>QUANTIDADE</th>
-                    <th>VALOR</th>
-                    <th>VOLUME</th>
+                    <th onclick="negociacaoController.ordena('data')">DATA</th>
+                    <th onclick="negociacaoController.ordena('quantidade')">QUANTIDADE</th>
+                    <th onclick="negociacaoController.ordena('valor')">VALOR</th>
+                    <th onclick="negociacaoController.ordena('volume')">VOLUME</th>
                 </tr>
             </thead>
 
             <tbody>
-            ${model.negociacoes.map(n => `
+            ${model.negociacoes
+              .map(
+                (n) => `
                 <tr>
                     <td>${DateHelper.dateToText(n.data)}</td>
                     <td>${n.quantidade}</td>
                     <td>${n.valor}</td>
                     <td>${n.volume}</td>
                 </tr>
-            `).join('')}
+            `
+              )
+              .join("")}
             </tbody>
 
             <tfoot>
                 <td colspan='3'></td>
                 <td>${
-            model.negociacoes.reduce((total, n) => total + n.volume, 0.0)
-            //abaixo outro modelo mais trabalhoso
-            // (() => {
-            //     let total = 0;
-            //     model.negociacoes.forEach(n => total += n.volume);
-            //     return total;
-            // })()
-            }</td>
+                  model.volumeTotal
+                  // modelo fora do model
+                  //model.negociacoes.reduce((total, n) => total + n.volume, 0.0)
+
+                  //abaixo outro modelo mais trabalhoso
+                  // (() => {
+                  //     let total = 0;
+                  //     model.negociacoes.forEach(n => total += n.volume);
+                  //     return total;
+                  // })()
+                }
+                </td>
             </tfoot>
         </table>
         `;
-    }
+  }
 }
